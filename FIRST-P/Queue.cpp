8d6cbefd9e1e01433bc142d3f6a772ffa924c1d7
MRomarAbdallah <omar.ab.gaming@gmail.com>
2024-11-06 22:34:35 +0200

    START

--- FIRST-P/Queue.cpp
+++ FIRST-P/Queue.cpp
@@ -54,6 +54,52 @@ int dequeue(Queue* q)
 }
 void cleanQueue(Queue* q)
 {
+    int i = 0;
+
+    for (i = 0; i < q->length;i++)
+    {
+        q->array[i] = -10;
+    }
+
+}
+bool isEmpty(Queue* s)
+{
+    int i = 0 , count = 0;
+
+    for (i = 0;i < s->length;i++)
+    {
+        if (s->array[i] == -10)
+        {
+            count++;
+        }
+
+    }
+
+    if (count == s->length)
+        return true;
+
+    else
+        return false;
+
+}
+bool isFull(Queue* s)
+{
+    int i = 0, sam = 0;
+    
+    for (i = 0;i < s->length;i++)
+    {
+        if (s->array[i] != -10)
+        {
+            sam++;
+        }
+
+    }
+
+    if (sam == s->length)
+        return true;
+
+    else
+        return false;
 
 }
 
